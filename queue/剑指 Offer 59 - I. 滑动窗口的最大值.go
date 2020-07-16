d22584dcb9e13497e7f9520d8e45b1aa5c975c99
davis.ying <davis.ying@gmail.com>
2020-07-16 15:44:48 +0800

    slidewindows

--- queue/剑指 Offer 59 - I. 滑动窗口的最大值.go
+++ queue/剑指 Offer 59 - I. 滑动窗口的最大值.go
@@ -90,87 +90,117 @@ func maxSlidingWindow(nums []int, k int) []int {
     return num
 }
 
+// 方法三： 双端队列
 
-// 方法三： 三指针+哨兵法
+// 双端队列和普通队列最大的不同在于，它允许我们在队列的头尾两端都能在 O(1)的时间内进行数据的查看、添加和删除
 
-//三指针
-//1. 使用左指针left和右指针right分别表示滑动窗口的左右边界
-//2. 使用最大值指针max指向当前的最大值,比较max 指针指向的 与left和right的大小
+// 可以利用一个双端队列来表示这个窗口。这个双端队列保存当前窗口中最大那个数的下标，
+// 双端队列新的头总是当前窗口中最大的那个数。 同时，有了这个下标，
+// 我们可以很快地知道新的窗口是否已经不再包含原来那个最大的数，如果不再包含， 我们就把旧的数从双端队列的头删除。
+// 按照这样的操作，不管窗口的长度是多长，因为数组里的每个数都分别被压入和弹出双端队列一次，所以我们可以在 O(n) 的时间里完成任务。
 
-// 哨兵
-//将left, right, max分别初始化为-1, k-2, -1的方式实现
-//如果将left, right, max分别初始化为0, k-1, 0, 是无法从始至终使用上述流程的
-
-
-// 更新max的2种情况
-//1. 随着左边界的迭代, max溜出滑动窗口的范围, 即left > max
-//2. 随着右边界的迭代, 右边新加入的值大于max位置的值
-
-
-
-
-
-// 方法四：  大顶堆+双指针
-
-import "container/heap"
-
-func maxSlidingWindow(nums []int, k int) []int {
-    if len(nums) == 0 || k == 0 {
+func maxSlidingWindow(nums []int,k int) []int {
+    if k < 1  {
         return nil
     }
-    intHeap := make(IntHeap, k)
-    copy(intHeap, nums[:k])
-    heap.Init(&intHeap)
-    results := make([]int, 0, len(nums))
-    results = append(results, intHeap[0])
-
-    for left, right := 0, k; right < len(nums); left, right = left + 1, right + 1 {
-        intHeap.Remove(nums[left])
-        heap.Push(&intHeap, nums[right])
-        results = append(results, intHeap[0])
+    if k < 2  {
+        return nums
     }
+    windows , res := []int{},[]int{}
+    for i,x  := range nums {
+        // 判断滑动窗口头部下标是否在i-k差值以内，大于的话，就说明windows的头结点是正常的滑动窗口范围
+        if i >= k && windows[0] <= i - k   {
+            windows = windows[1:]
+        }
+        // 判断尾部元素和新入队元素的大小，如果新入队元素大于所有windows的元素，则windows 清理尾部元素
+        for len(windows) > 0  && nums[windows[len(windows)-1]] < x {
+            windows = windows[:len(windows)-1]
+        }
+        // 把下标信息入队
+        windows = append(windows,i)
 
-    return results
-}
-
-type IntHeap []int
-
-func (h *IntHeap) Remove(x int) {
-    for i := 0; i < len(*h); i++ {
-        if (*h)[i] == x {
-            *h = append((*h)[:i], (*h)[i + 1:]...)
-            break
+        // 获取每次的windows的开头元素
+        if i >= k -1 {
+            res = append(res,nums[windows[0]])
         }
     }
-
-    heap.Init(h)
+    return res
 }
 
-func (h IntHeap) Len() int {
-    return len(h)
+
+//方法四： 动态规划
+
+//直觉
+//
+//这是另一个 {O}(N)O(N) 的算法。本算法的优点是不需要使用 数组 / 列表 之外的任何数据结构。
+//
+//算法的思想是将输入数组分割成有 k 个元素的块。
+//若 n % k != 0，则最后一块的元素个数可能更少
+
+// 开头元素为 i ，结尾元素为 j 的当前滑动窗口可能在一个块内，也可能在两个块中
+
+//算法十分直截了当：
+//
+//从左到右遍历数组，建立数组 left。
+//
+//从右到左遍历数组，建立数组 right。
+//
+//建立输出数组 max(right[i], left[i + k - 1])，其中 i 取值范围为 (0, n - k + 1)。
+
+
+// 复杂度分析
+//
+//时间复杂度：{O}(N)O(N)，我们对长度为 N 的数组处理了 3次。
+//
+//空间复杂度：{O}(N)O(N)，用于存储长度为 N 的 left 和 right 数组，以及长度为 N - k + 1的输出数组。
+
+func maxSlidingWindow(nums []int,k int) []int {
+    // 边界条件
+    if k < 1 || k ==0 ||nums == nil   {
+        return nil
+    }
+    if k < 2  {
+        return nums
+    }
+    res := []int{}
+    length := len(nums)
+    // 创建left、right 数组
+    left := make([]int,length)
+    right := make([]int,length)
+
+    // 从左到右，生成left数组
+    for i:=0;i<length;i++ {
+        if i % k == 0 {
+            left[i] = nums[i]
+        } else {
+            left[i] = MaxInt(left[i-1],nums[i])
+        }
+    }
+    // 从右到左，生成right数组
+    right[length-1] = nums[length-1]
+
+    for i:= length-2;i>=0;i-- {
+        if (i+1) %k == 0 {
+            right[i] = nums[i]
+        } else {
+            right[i] = MaxInt(right[i+1],nums[i])
+        }
+    }
+    // 从0到length-k+1,进行比较left[j],right[i]
+    for i := 0;i<length-k+1;i++{
+        res =append(res,MaxInt(left[i+k-1],right[i]))
+    }
+    return res
+}
+func MaxInt(i,j int) int {
+    if i < j {
+        return j
+    }
+    return i
 }
 
-func (h IntHeap) Less(i int, j int) bool {
-    return h[i] >= h[j]
-}
 
-func (h IntHeap) Swap(i int, j int) {
-    h[i], h[j] = h[j], h[i]
-}
 
-func (h *IntHeap) Push(x interface{}) {
-    *h = append(*h, x.(int))
-}
 
-func (h *IntHeap) Pop() interface{} {
-    length := len(*h)
-    val := (*h)[length - 1]
-    *h = (*h)[: length - 1]
 
-    return val
-}
 
-//作者：jihonghe
-//链接：https://leetcode-cn.com/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof/solution/zui-da-dui-shuang-zhi-zhen-by-jihonghe/
-//来源：力扣（LeetCode）
-//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
