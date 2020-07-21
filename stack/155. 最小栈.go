88f20177a1ad496fd044a3f4b3a2d98d9b26ba94
davis.ying <davis.ying@gmail.com>
2020-07-21 18:02:59 +0800

    modify stack

--- stack/155. 最小栈.go
+++ stack/155. 最小栈.go
@@ -33,13 +33,16 @@ package main
 //pop、top 和 getMin 操作总是在 非空栈 上调用。
 
 
-// 思考方式 使用辅助栈来达到O(1) 获取GetMin,栈的实现是用切片实现的
+// 思考方式 使用辅助栈来达到O(1) 获取GetMin,栈的实现是用切片实现的,
+// 当栈用链表实现的时候，就可以增加一个Min的值
+// 看到有使用一个栈来实现求最小值的，思路挺新奇的
 // 对于栈来说，如果一个元素 a 在入栈时，栈里有其它的元素 b, c, d，那么无论这个栈在之后经历了什么操作，只要 a 在栈中，b, c, d 就一定在栈中，因为在 a 被弹出之前，b, c, d 不会被弹出。
 //
 //因此，在操作过程中的任意一个时刻，只要栈顶的元素是 a，那么我们就可以确定栈里面现在的元素一定是 a, b, c, d。
 //
 //那么，我们可以在每个元素 a 入栈时把当前栈的最小值 m 存储起来。在这之后无论何时，如果栈顶元素是 a，我们就可以直接返回存储的最小值 m。
 //
+
 
 // 方法一:    辅助栈
 
@@ -94,3 +97,61 @@ func (this *MinStack) GetMin() int {
 }
 
 
+// 方法二： 链表带有min属性
+
+type MinStack struct {
+	Value int
+	Min   int
+	Next *MinStack
+}
+
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	head := MinStack{-1,-1,nil}
+	return head
+}
+
+func (this *MinStack) Push(x int)  {
+	head := this
+	if head.Next == nil {
+		node := &MinStack{}
+		node.Value = x
+		node.Min = x
+		head.Next = node
+		return
+	}
+	node := &MinStack{}
+	node.Value = x
+	next := head.Next
+	node.Min = MinNum(x,next.Min)
+	node.Next = next
+	head.Next = node
+}
+
+
+func (this *MinStack) Pop()  {
+	this.Next = this.Next.Next
+}
+
+
+func (this *MinStack) Top() int {
+	head := this
+	return head.Next.Value
+}
+
+
+func (this *MinStack) GetMin() int {
+	head := this
+	if head.Next == nil {
+		return -1
+	}
+	return head.Next.Min
+}
+
+func MinNum(x,y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
